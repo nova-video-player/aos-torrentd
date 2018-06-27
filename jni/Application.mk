3b8406d4273aaeabe8f83efb53b8d4ec06c2657e
Marc de Courville <marc@courville.org>
2018-06-27 09:16:52 +0200

    ndk 17 modifications WIP

--- jni/Application.mk
+++ jni/Application.mk
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-APP_STL := gnustl_static
-NDK_TOOLCHAIN_VERSION := 4.9
+APP_STL := c++_static
 APP_ABI := x86 armeabi-v7a
 APP_PLATFORM := android-17
