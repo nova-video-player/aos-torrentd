392d4d3ccdd5a06236267f69395fb6be5bbe0b08
Courville Software <software@courville.org>
2021-01-23 20:23:32 +0100

    fix include path for new libtorrent dir

--- Android.mk
+++ Android.mk
@@ -22,7 +22,7 @@ LOCAL_MODULE:= torrentd
 
 LOCAL_CFLAGS += -I$(REPO_TOP_DIR)/native/boost/boost_1_72_0 -std=c++11
 LOCAL_LDLIBS= $(REPO_TOP_DIR)/native/boost/boost_1_72_0-$(TARGET_ARCH_ABI)/boost/bin.v2/libs/system/build/clang-$(os)-android/release/link-static/target-os-android/visibility-hidden/libboost_system.a
-LOCAL_CFLAGS += -I$(REPO_TOP_DIR)/native/libtorrent-android-builder/include
+LOCAL_CFLAGS += -I$(REPO_TOP_DIR)/native/libtorrent-android-builder/libtorrent/include
 LOCAL_LDLIBS += $(REPO_TOP_DIR)/native/boost/boost_1_72_0-$(TARGET_ARCH_ABI)/torrent/clang-$(os)-android/release/link-static/target-os-android/threading-multi/libtorrent.a
 
 LOCAL_LDLIBS += $(android_ndk)/sources/cxx-stl/llvm-libc++/libs/$(TARGET_ARCH_ABI)/libc++_static.a
