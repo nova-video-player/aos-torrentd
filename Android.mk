034ce7fc872c4f0a64b23fa8910f02a9f98d0285
Courville Software <software@courville.org>
2024-01-08 19:36:37 +0100

    torrentd: adapt to new boost archive naming
    See https://github.com/nova-video-player/aos-Fdroid/issues/2

--- Android.mk
+++ Android.mk
@@ -23,10 +23,10 @@ LOCAL_SRC_FILES := torrentd.cpp httpd.cpp \
 
 LOCAL_MODULE:= torrentd
 
-LOCAL_CFLAGS += -I$(REPO_TOP_DIR)/native/boost/boost_1_84_0
-LOCAL_LDLIBS= $(REPO_TOP_DIR)/native/boost/boost_1_84_0-$(TARGET_ARCH_ABI)/boost/bin.v2/libs/system/build/clang-$(os)-android/release/cxxstd-14-iso/link-static/target-os-android/threading-multi/visibility-hidden/libboost_system.a
+LOCAL_CFLAGS += -I$(REPO_TOP_DIR)/native/boost/boost-1.84.0
+LOCAL_LDLIBS= $(REPO_TOP_DIR)/native/boost/boost-1.84.0-$(TARGET_ARCH_ABI)/boost/bin.v2/libs/system/build/clang-$(os)-android/release/cxxstd-14-iso/link-static/target-os-android/threading-multi/visibility-hidden/libboost_system.a
 LOCAL_CFLAGS += -I$(REPO_TOP_DIR)/native/libtorrent-android-builder/libtorrent/include
-LOCAL_LDLIBS += $(REPO_TOP_DIR)/native/boost/boost_1_84_0-$(TARGET_ARCH_ABI)/torrent/clang-$(os)-android/release/cxxstd-14-iso/link-static/target-os-android/threading-multi/visibility-hidden/libtorrent-rasterbar.a
+LOCAL_LDLIBS += $(REPO_TOP_DIR)/native/boost/boost-1.84.0-$(TARGET_ARCH_ABI)/torrent/clang-$(os)-android/release/cxxstd-14-iso/link-static/target-os-android/threading-multi/visibility-hidden/libtorrent-rasterbar.a
 LOCAL_LDLIBS += $(REPO_TOP_DIR)/native/openssl-android-builder/dist-$(TARGET_ARCH_ABI)/lib/libssl.a
 LOCAL_LDLIBS += $(REPO_TOP_DIR)/native/openssl-android-builder/dist-$(TARGET_ARCH_ABI)/lib/libcrypto.a
 
